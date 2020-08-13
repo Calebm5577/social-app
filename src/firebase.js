301c1fa92c923adcdfba83ab99343e8d5b723dbb
Calebm5577 <60674618+Calebm5577@users.noreply.github.com>
2020-08-13 11:42:42 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAH-JLst5OAcoNvwjuaSyD7sprU4SGfXC4",
-    authDomain: "discord-style-chat-app.firebaseapp.com",
-    databaseURL: "https://discord-style-chat-app.firebaseio.com",
-    projectId: "discord-style-chat-app",
-    storageBucket: "discord-style-chat-app.appspot.com",
-    messagingSenderId: "596026101224",
-    appId: "1:596026101224:web:97120654fadcefbb08a85d",
-    measurementId: "G-QP3EKRZRSK"
+    apiKey: "*********************************",
+    authDomain: "***********************************",
+    databaseURL: "*******************************",
+    projectId: "*******************************",
+    storageBucket: "*******************************",
+    messagingSenderId: "*******************************",
+    appId: "*******************************",
+    measurementId: "*******************************"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
